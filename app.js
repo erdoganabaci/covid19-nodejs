7c8b74c916a0e0dee601e2c009fe8eaf463b31fd
erdoganabaci <erdoganabaci97@gmail.com>
2020-06-26 19:31:32 +0300

    earthquake added

--- app.js
+++ app.js
@@ -20,9 +20,23 @@ class CoronaWorld {
     }
 }
 
+class EartquakeTurkey {
+    constructor(cityName, placeName, latitute, longitute, magnitute, depth, date, time) {
+        this.cityName = cityName;
+        this.placeName = placeName;
+        this.latitute = latitute;
+        this.longitute = longitute;
+        this.magnitute = magnitute;
+        this.depth = depth;
+        this.date = date;
+        this.time = time;
+
+    }
+}
+
 const PORT = process.env.PORT || 5000;
 
-console.log("test");
+console.log("Server is running!");
 
 //Templete Engine Middleware
 app.engine(
@@ -121,6 +135,73 @@ app.get("/", function(req, res) {
         }
     );
 });
+
+
+app.get("/deprem", function(req, res) {
+    //var coronaTest = getData();
+    /*res.render('pages/homepage', {
+          name: 'xyz'
+      })*/
+
+    /*var url =
+        "http://developer.cumtd.com/api/v2.2/json/GetStop?" +
+        "key=d99803c970a04223998cabd90a741633" +
+        "&stop_id=it";*/
+    var url = "https://turkiyedepremapi.herokuapp.com/api?min=4.2";
+    var dateFromJson;
+    var timeFromJson;
+    var latituteFromJson;
+    var longituteFromJson;
+    var depthFromJson;
+    var magnituteFromJson;
+    var placeFromJson;
+    var cityFromJson;
+    request({
+            url: url,
+            json: true,
+        },
+        function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var myJSON = JSON.stringify(body);
+                var objectValue = JSON.parse(myJSON);
+                var allArrayData = objectValue[0];
+                console.log(objectValue[0]);
+                console.log(allArrayData['tarih']);
+
+                dateFromJson = allArrayData['tarih'];
+                timeFromJson = allArrayData['saat'];
+                latituteFromJson = allArrayData['enlem:'];
+                longituteFromJson = allArrayData['boylam'];
+                depthFromJson = allArrayData['derinlik'];
+                magnituteFromJson = allArrayData['buyukluk'];
+                placeFromJson = allArrayData['yer'];
+                cityFromJson = allArrayData['sehir'];
+
+
+                let eartquakeTurkey = new EartquakeTurkey(cityFromJson, placeFromJson, latituteFromJson, longituteFromJson, magnituteFromJson, depthFromJson, dateFromJson, timeFromJson);
+
+                //var globalJson = objectValue['Countries'];
+
+
+                //console.log(globalJson[0]);
+
+                //console.log(objectValue1["Country"]);
+                //console.log(myJSON);
+                //var myJSON = JSON.stringify(globalJson);
+                //var objectValue1 = JSON.parse(myJSON1);
+                /*for (var attributename in myJSON1) {
+                    console.log(attributename + ": " + myJSON1[attributename]);
+                }*/
+                res.render('pages/earthquakepage', {
+                    eartquakeTurkey: eartquakeTurkey
+                });
+
+
+            }
+        }
+    );
+});
+
 
 app.use((req, res) => {
     //res.render("static/404");
